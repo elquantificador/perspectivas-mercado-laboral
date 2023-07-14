f8fa7fcae779b4f0ee8fd4277ed1c8eaf15e76c5
sebastian <sebas262729@gmail.com>
2023-07-14 11:19:16 -0500

    agreando codigo (arrange)

--- code/analisis_ress.R
+++ code/analisis_ress.R
@@ -12,7 +12,7 @@ View(ress_raw)
 df_raw <- ress_raw %>%  transmute(ano, mes, provincia, edad, sueldo, dias, sector_afiliacion, tipo_empleador, tamano_empleo, ciiu4_1) %>% filter(provincia %in% seq(1:24), sueldo>0)
 View(df_raw)
 # agrupando la base de 2022 -----
-df_2022 <- df_raw %>% filter(ano == 2022, sueldo >= 425, dias == 30) %>% group_by(provincia, ciiu4_1) %>% summarize(media_sueldo = mean(sueldo, na.rm = TRUE))
+df_2022 <- df_raw %>% filter(ano == 2022, sueldo >= 425, dias == 30) %>% group_by(provincia, ciiu4_1) %>% summarize(media_sueldo = mean(sueldo, na.rm = TRUE)) %>% arrange(desc(media_sueldo))
 
 # agrupando la base de 2023 -----
-df_2023 <- df_raw %>% filter(ano == 2023, sueldo >= 425, dias == 30) %>% group_by(provincia, ciiu4_1) %>% summarize(media_sueldo = mean(sueldo, na.rm = TRUE))
+df_2023 <- df_raw %>% filter(ano == 2023, sueldo >= 425, dias == 30) %>% group_by(provincia, ciiu4_1) %>% summarize(media_sueldo = mean(sueldo, na.rm = TRUE)) %>% arrange(desc(media_sueldo))
