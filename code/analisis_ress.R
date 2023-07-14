458fdd68874ff566b192b8333ae13bbb968facfd
Daniel Sánchez <dsanchezp998@gmail.com>
2023-07-14 16:14:44 -0700

    quitar transmute

--- code/analisis_ress.R
+++ code/analisis_ress.R
@@ -1,18 +1,42 @@
 # Repositorio REES
-# librerias -----
+
+# Preliminares --------------------------------------------------------------------------------------------
+
+# Cargar librerias
+
 library(dplyr)
-library(plyr)
 library(readxl)
 library(ggplot2)
 library(readr)
-# Descargando la Data -----
+
+# Cargando datos ------------------------------------------------------------------------------------------
+
 source("code/ress_download.R")
-View(ress_raw)
-# Analisis -----
-df_raw <- ress_raw %>%  transmute(ano, mes, provincia, edad, sueldo, dias, sector_afiliacion, tipo_empleador, tamano_empleo, ciiu4_1) %>% filter(provincia %in% seq(1:24), sueldo>0)
-View(df_raw)
+
+# Guardar la base de datos en la computadora con save(ress_raw, file = 'data/base_iess.Rdata')
+# Cargar la base de datos del archivo Rdata con load('data/base_iess.Rdata')
+
+# Análisis ------------------------------------------------------------------------------------------------
+
+df_raw <- 
+  ress_raw %>%  
+  select(ano, mes, provincia, edad, sueldo , dias, sector_afiliacion, tipo_empleador, tamano_empleo, ciiu4_1) %>% 
+  filter(provincia %in% seq(1:24), sueldo>0)
+
 # agrupando la base de 2022 -----
-df_2022 <- df_raw %>% filter(ano == 2022, sueldo >= 425, dias == 30) %>% group_by(provincia, ciiu4_1) %>% summarize(media_sueldo = mean(sueldo, na.rm = TRUE)) %>% arrange(desc(media_sueldo))
+
+df_2022 <- 
+  df_raw %>% 
+  filter(ano == 2022, sueldo >= 425, dias == 30) %>% 
+  group_by(provincia, ciiu4_1) %>% 
+  summarize(media_sueldo = mean(sueldo, na.rm = TRUE)) %>% 
+  arrange(desc(media_sueldo))
 
 # agrupando la base de 2023 -----
-df_2023 <- df_raw %>% filter(ano == 2023, sueldo >= 425, dias == 30) %>% group_by(provincia, ciiu4_1) %>% summarize(media_sueldo = mean(sueldo, na.rm = TRUE)) %>% arrange(desc(media_sueldo))
+
+df_2023 <- 
+  df_raw %>% 
+  filter(ano == 2023, sueldo >= 425, dias == 30) %>% 
+  group_by(provincia, ciiu4_1) %>% 
+  summarize(media_sueldo = mean(sueldo, na.rm = TRUE)) %>% 
+  arrange(desc(media_sueldo))
