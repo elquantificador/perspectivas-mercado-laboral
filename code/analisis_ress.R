268ddd22f72ce6ebe2e5a6cbd01d43e80b5f2f60
sebastian <sebas262729@gmail.com>
2023-07-19 14:13:39 -0500

    limpiando los graficos

--- code/analisis_ress.R
+++ code/analisis_ress.R
@@ -22,55 +22,26 @@ df_raw <-
   ress_raw %>%  
   select(ano, mes, provincia, edad, sueldo , dias, empleo_total, ciiu4_1, empleo) %>% 
   filter(provincia %in% seq(1:24), sueldo>0, empleo_total %in% c(1,2)) %>%
-  mutate(sector = factor(empleo_total, levels = c(1:2), labels = c("Privado", "Publico")),
+  mutate(sector = factor(empleo_total, levels = c(1:2), labels = c("Privado", "Público")),
          prov_fct = factor(provincia, levels = c(1:24), 
                            labels = c("Azuay","Bolívar","Cañar","Carchi","Cotopaxi","Chimborazo",
                                       "El Oro","Esmeraldas","Guayas","Imbabura","Loja","Los Ríos",
                                       "Manabí","Morona Santiago","Napo","Pastaza","Pichincha",
                                       "Tungurahua","Z.Chinchipe","Galápagos","Sucumbíos","Orellana",
-                                      "Santo Domingo de los Tsáchilas","Santa Elena")),
-         ciiu4_1_fct =factor(ciiu4_1, levels = c(1:22),
-                             labels = c("Agricultura, ganadería,  silvicultura y pesca",
-                                        "Explotación de minas y canteras",
-                                        "Industrias manufactureras",
-                                        "Suministro de electricidad, gas, vapor y aire acondicionado",
-                                        "Distribución de agua; alcantarillado, gestión de desechos y actividades de saneamiento.",
-                                        "Construcción",
-                                        "Comercio al por mayor y al por menor; reparación de vehículos automotores y motocicletas",
-                                        "Transporte y almacenamiento",
-                                        "Actividades de alojamiento y de servicio de comidas",
-                                        "Información y comunicación",
-                                        "Actividades financieras y de seguros",
-                                        "Actividades inmobiliarias",
-                                        "Actividades profesionales, científicas y técnicas",
-                                        "Actividades de servicios administrativos y de apoyo",
-                                        "Administración pública y defensa; planes de seguridad social de afiliación obligatoria",
-                                        "Enseñanza",
-                                        "Actividades de atención de la salud humana y de asistencia social",
-                                        "Artes, entretenimiento y recreación",
-                                        "Otras actividades de servicios",
-                                        "Actividades de los hogares como empleadores; actividades no diferenciadas de los hogares como productores de bienes y servicios para uso propio",
-                                        "Actividades de organizaciones y órganos extraterritoriales",
-                                        "CIIU no determinada")))
+                                      "Santo Domingo de los Tsáchilas","Santa Elena")))
 
-# agrupando la base de 2022 d-----
+# agrupando la base de 2022 -----
 
 df_2022_p <- 
   df_raw %>% 
-  filter(ano == 2022, sueldo >= 425, dias == 30, 
+  filter(ano == 2022, mes == 12, 
          prov_fct %in% c("Azuay","Pichincha","Guayas","El Oro","Manabí")) %>%
   group_by(prov_fct,sector) %>% 
   summarize(empleo = n())
 
-df_2022_sp <- df_raw %>% 
-  filter(ano == 2022, sueldo >= 425, dias == 30) %>%
-  group_by(provincia, mes) %>%
-  summarize(mediana_sueldo = median(sueldo, na.rm = TRUE)) %>%
-  arrange(desc(mediana_sueldo))
-
 df_2022_c <- 
   df_raw %>% 
-  filter(ano == 2022) %>% 
+  filter(ano == 2022, mes == 12) %>% 
   group_by(ciiu4_1) %>% 
   summarize(mediana_sueldo = median(sueldo, na.rm = TRUE), empleo = n()) %>% 
   arrange(desc(mediana_sueldo))
@@ -79,14 +50,14 @@ df_2022_c <-
 
 df_2023_p <- 
   df_raw %>% 
-  filter(ano == 2023, sueldo >= 450, dias == 30, 
+  filter(ano == 2023, mes == 12, 
          prov_fct %in% c("Azuay","Pichincha","Guayas","El Oro","Manabí")) %>%
   group_by(prov_fct,sector) %>% 
   summarize(empleo = n())
 
 df_2023_c <- 
   df_raw %>% 
-  filter(ano == 2023, sueldo >= 425, dias == 30) %>% 
+  filter(ano == 2023, mes == 12) %>% 
   group_by(ciiu4_1) %>% 
   summarize(mediana_sueldo = median(sueldo, na.rm = TRUE), empleo = n()) %>% 
   arrange(desc(mediana_sueldo))
@@ -103,20 +74,14 @@ theme_iess_2 <-
 # visualizacion 2022 -----
 empleo_2022_c
 empleo_2022_p
-mediana_2022_p
-
-mediana_2022_p <- ggplot(df_2022_sp %>% filter(provincia %in% c(17,9,1,13,7)), 
-                         aes(x = as.factor(mes),y = mediana_sueldo, color = as.factor(provincia), 
-                             group = 5)) +
-  geom_line()
 
 empleo_2022_c <- ggplot(df_2022_c, aes(x = reorder(as.factor(ciiu4_1), -empleo), empleo, fill = empleo)) +
   geom_col(fill = "#647A8F",
            width = 0.8,
            color = "black")+
-  labs(x = "actividad productiva", 
-       y = "numero de empleos", 
-       title = "Numero de empleos por actividad productiva 2022",
+  labs(x = "Actividad productiva", 
+       y = "Número de empleos", 
+       title = "Número de empleos por actividad productiva 2022",
        subtitle = "Fuente : IESS"
   ) +
   geom_text(aes(label = empleo), color = "black", size = 2, vjust = -0.5) +
@@ -132,10 +97,13 @@ empleo_2022_p <- ggplot(df_2022_p,
            color = "black") +
   scale_fill_manual(values =c("#647A8F","#FFAC8E")) +
   labs(x = "Provincia",
-       y = "numero de empleos",
-       title = "Numero de empleos por provincia y sector 2022",
+       y = "Número de empleos",
+       title = "Número de empleos por provincia y sector 2022",
        subtitle = "Fuente : IESS",
        fill = "Sector") +
+  geom_text(aes(label = empleo),
+            color = 'white',
+            vjust = 1.5) +
   theme_iess_2
 
 # visualizacion 2023 -----
@@ -148,9 +116,9 @@ empleo_2023_c <- ggplot(df_2023_c %>% filter(ciiu4_1 != "Z0_Nocla_CIIU"),
   geom_col(fill = "#647A8F",
            width = 0.8,
            color = "black")+
-  labs(x = "actividad productiva", 
-       y = "numero de empleos", 
-       title = "Numero de empleos por actividad productiva hasta marzo 2023",
+  labs(x = "Actividad productiva", 
+       y = "Número de empleos", 
+       title = "Número de empleos por actividad productiva hasta marzo 2023",
        subtitle = "Fuente : IESS"
   ) +
   geom_text(aes(label = empleo), color = "black", size = 2,  vjust = -0.5) +
@@ -166,8 +134,8 @@ empleo_2023_p <- ggplot(df_2023_p,
            color = "black") +
   scale_fill_manual(values =c("#647A8F","#FFAC8E")) +
   labs(x = "Provincia",
-       y = "numero de empleos",
-       title = "Numero de empleos por provincia y sector hasta marzo 2023",
+       y = "Número de empleos",
+       title = "Número de empleos por provincia y sector hasta marzo 2023",
        subtitle = "Fuente : IESS",
        fill = "Sector") +
   theme_iess_2
