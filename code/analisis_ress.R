b7318960f299b11262d83dd00a97168317700d49
sebastian <sebas262729@gmail.com>
2023-07-19 16:47:14 -0500

    Actualizando graficos

--- code/analisis_ress.R
+++ code/analisis_ress.R
@@ -8,6 +8,7 @@ library(dplyr)
 library(readxl)
 library(ggplot2)
 library(readr)
+library(forcats)
 
 # Cargando datos ------------------------------------------------------------------------------------------
 
@@ -28,7 +29,8 @@ df_raw <-
                                       "El Oro","Esmeraldas","Guayas","Imbabura","Loja","Los Ríos",
                                       "Manabí","Morona Santiago","Napo","Pastaza","Pichincha",
                                       "Tungurahua","Z.Chinchipe","Galápagos","Sucumbíos","Orellana",
-                                      "Santo Domingo de los Tsáchilas","Santa Elena")))
+                                      "Santo Domingo de los Tsáchilas","Santa Elena")),
+         ciiu4_1_fct = as.factor(ciiu4_1))
 
 # agrupando la base de 2022 -----
 
@@ -42,7 +44,29 @@ df_2022_p <-
 df_2022_c <- 
   df_raw %>% 
   filter(ano == 2022, mes == 12) %>% 
-  group_by(ciiu4_1) %>% 
+  mutate(ciiu4_1_fct = fct_recode(ciiu4_1_fct,
+  "Agricultura, ganadería, silvicultura y pesca" = "A",
+  "Explotación de minas y canteras" = "B",
+  "Industrias manufactureras" = "C",
+  "Suministro de electricidad, gas, vapor y aire acondicionado" = "D",
+  "Distribución de agua; alcantarillado, gestión de desechos y actividades de saneamiento" = "E",
+  "Construcción" = "F",
+  "Comercio al por mayor y al por menor; reparación de vehículos automotores y motocicletas" = "G",
+  "Transporte y almacenamiento" = "H",
+  "Actividades de alojamiento y de servicio de comidas" = "I",
+  "Información y comunicación" = "J",
+  "Actividades financieras y de seguros" = "K",
+  "Actividades inmobiliarias" = "L",
+  "Actividades profesionales, científicas y técnicas" = "M",
+  "Actividades de servicios administrativos y de apoyo" = "N",
+  "Administración pública y defensa; planes de seguridad social de afiliación obligatoria" = "O",
+  "Enseñanza" = "P",
+  "Actividades de atención de la salud humana y de asistencia social" = "Q",
+  "Artes, entretenimiento y recreación" = "R",
+  "Otras actividades de servicios" = "S",
+  "Actividades de organizaciones y órganos extraterritoriales" = "U",
+  "No determinada" = "Z0_Nocla_CIIU")) %>%
+  group_by(ciiu4_1_fct) %>% 
   summarize(mediana_sueldo = median(sueldo, na.rm = TRUE), empleo = n()) %>% 
   arrange(desc(mediana_sueldo))
 
@@ -58,7 +82,29 @@ df_2023_p <-
 df_2023_c <- 
   df_raw %>% 
   filter(ano == 2023, mes == 12) %>% 
-  group_by(ciiu4_1) %>% 
+  mutate(ciiu4_1_fct = fct_recode(ciiu4_1_fct,
+                                  "Agricultura, ganadería, silvicultura y pesca" = "A",
+                                  "Explotación de minas y canteras" = "B",
+                                  "Industrias manufactureras" = "C",
+                                  "Suministro de electricidad, gas, vapor y aire acondicionado" = "D",
+                                  "Distribución de agua; alcantarillado, gestión de desechos y actividades de saneamiento" = "E",
+                                  "Construcción" = "F",
+                                  "Comercio al por mayor y al por menor; reparación de vehículos automotores y motocicletas" = "G",
+                                  "Transporte y almacenamiento" = "H",
+                                  "Actividades de alojamiento y de servicio de comidas" = "I",
+                                  "Información y comunicación" = "J",
+                                  "Actividades financieras y de seguros" = "K",
+                                  "Actividades inmobiliarias" = "L",
+                                  "Actividades profesionales, científicas y técnicas" = "M",
+                                  "Actividades de servicios administrativos y de apoyo" = "N",
+                                  "Administración pública y defensa; planes de seguridad social de afiliación obligatoria" = "O",
+                                  "Enseñanza" = "P",
+                                  "Actividades de atención de la salud humana y de asistencia social" = "Q",
+                                  "Artes, entretenimiento y recreación" = "R",
+                                  "Otras actividades de servicios" = "S",
+                                  "Actividades de organizaciones y órganos extraterritoriales" = "U",
+                                  "No determinada" = "Z0_Nocla_CIIU")) %>%
+  group_by(ciiu4_1_fct) %>% 
   summarize(mediana_sueldo = median(sueldo, na.rm = TRUE), empleo = n()) %>% 
   arrange(desc(mediana_sueldo))
 
@@ -75,19 +121,19 @@ theme_iess_2 <-
 empleo_2022_c
 empleo_2022_p
 
-empleo_2022_c <- ggplot(df_2022_c, aes(x = reorder(as.factor(ciiu4_1), -empleo), empleo, fill = empleo)) +
-  geom_col(fill = "#647A8F",
+empleo_2022_c <- ggplot(df_2022_c, aes(ciiu4_1_fct, empleo)) +
+  geom_bar(stat = "identity",
+           fill = "#647A8F",
            width = 0.8,
-           color = "black")+
-  labs(x = "Actividad productiva", 
-       y = "Número de empleos", 
+           color = "black") +
+  coord_flip() +
+  labs(x = "Número de empleos", 
+       y = "Actividad productiva", 
        title = "Número de empleos por actividad productiva 2022",
-       subtitle = "Fuente : IESS"
-  ) +
-  geom_text(aes(label = empleo), color = "black", size = 2, vjust = -0.5) +
+       subtitle = "Fuente : IESS")+
   theme_iess_2 +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank())
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
 
 empleo_2022_p <- ggplot(df_2022_p,
                         aes(reorder(prov_fct, -empleo), empleo, 
@@ -111,20 +157,22 @@ empleo_2022_p <- ggplot(df_2022_p,
 empleo_2023_c
 empleo_2023_p 
 
-empleo_2023_c <- ggplot(df_2023_c %>% filter(ciiu4_1 != "Z0_Nocla_CIIU"),
-                        aes(x = reorder(as.factor(ciiu4_1), -empleo), empleo, fill = empleo)) +
-  geom_col(fill = "#647A8F",
+empleo_2023_c <- ggplot(df_2023_c, aes(ciiu4_1_fct, empleo)) +
+  geom_bar(stat = "identity",
+           fill = "#647A8F",
            width = 0.8,
-           color = "black")+
-  labs(x = "Actividad productiva", 
-       y = "Número de empleos", 
+           color = "black") +
+  coord_flip() +
+  labs(x = "Número de empleos", 
+       y = "Actividad productiva", 
        title = "Número de empleos por actividad productiva hasta marzo 2023",
-       subtitle = "Fuente : IESS"
-  ) +
-  geom_text(aes(label = empleo), color = "black", size = 2,  vjust = -0.5) +
+       subtitle = "Fuente : IESS") +
+  geom_text(aes(label = empleo, y = empleo + 0.3), color = "#FFAC8E", 
+            size = 3, position = position_dodge(0.9),
+            vjust = 1) +
   theme_iess_2 +
-  theme(axis.text.y = element_blank(),
-        axis.ticks.y = element_blank())
+  theme(axis.text.x = element_blank(),
+        axis.ticks.x = element_blank())
 
 empleo_2023_p <- ggplot(df_2023_p,
                         aes(reorder(prov_fct, -empleo), empleo, 
